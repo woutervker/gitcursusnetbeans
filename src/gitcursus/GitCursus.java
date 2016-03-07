7b95efdc6ceae566cbe713ee36cd978df429de3e
Wouter <wouter.vker@hotmail.be>
2016-03-07 15:10:04 +0100

    bonjour

--- src/gitcursus/GitCursus.java
+++ src/gitcursus/GitCursus.java
@@ -15,7 +15,7 @@ public class GitCursus {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("bonjour");
     }
     
 }
