004bc3d904b767c3e79632abbb594fd58a6b22aa
woutervker <wouter.vker@hotmail.be>
2016-03-07 15:28:50 +0100

    iedereen naar allemaal

--- src/gitcursus/GitCursus.java
+++ src/gitcursus/GitCursus.java
@@ -15,7 +15,7 @@ public class GitCursus {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hallo");
+        System.out.println("hallo allemaal");
     }
     
 }
